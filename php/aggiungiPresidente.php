aa91ee092c25c0631583f9ffa682c140acb0510f
osky104 <oscar.farina@buonarroti.tn.it>
2025-11-25 14:38:14 +0100

    modifiche presidenti e js

--- php/aggiungiPresidente.php
+++ php/aggiungiPresidente.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+if(isset($_SESSION["loggato"])){
+    if(!$_SESSION["loggato"]){
+        header("Location: formModifiche.php");
+    }
+}else{
+    header("Location: formModifiche.php");
+}
+
 
 ?>
 
@@ -8,12 +16,23 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Aggiungi presidente</title>
 </head>
 <body>
-    <h2>Inserisci i dati per aggiungere un presidente</h2>
-    <form action="" >
-        <label for="nome"></label>
+    <h1>Aggiungi un presidente</h1>
+
+    <form action="" method="post" id="formPresidente">
+        <label>Nome: </label>
+        <input type="text" id="nome" name="nome" placeholder="Inserisci nome e cognome"><br><br>
+        <label>Mandato: </label>
+        <input type="number" id="dataInizio" name="dataInizio" min="" max="" >
+        <label> - </label>
+        <input type="number" id="dataFine" name="dataFine" min="" max=""><br><br>
+
+        <button type="submit">Aggiungi</button>
     </form>
+
+    <script src="../js/presidenti.js"></script>
+    
 </body>
 </html>
